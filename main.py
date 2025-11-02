71b472d92d389810c6448354fb4b0be32415fab6
Craig Rossiter <crossiter0@gmail.com>
2025-11-02 16:20:21 -0600

    added changes to allow for arguments to be added to which book to analyze

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import split_text, count_characters, split_char, generate_report
+import sys
+
 
 def get_book_text(path):
     with open(path) as book:
@@ -9,7 +11,13 @@ def main(path):
     text = get_book_text(path)
     count = count_characters(text)
     print(generate_report(text, path))
-    
-book = "books/frankenstein.txt"
-text = get_book_text(book)
+
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+
+book = sys.argv[1]
 main(book)
+
